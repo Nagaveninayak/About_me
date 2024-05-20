e63dfdae9027e6e98ce8b5dd11c76230b6ce5517
Nagaveni <nagaveninayakn@gmail.com>
2024-05-20 21:55:57 +0530

    Updated the project component to show nav and card

---
+++ constants/EducationConstants.ts
@@ -0,0 +1,9 @@
+import codingImage from "../public/images/codingImage.jpeg";
+
+export const IMAGE_CONSTANTS = [
+    {title: "Todo", imagePath: codingImage },
+    {title: "Todo 1", imagePath: codingImage },
+    {title: "Todo 2", imagePath: codingImage },
+    {title: "Todo 3", imagePath: codingImage },
+    {title: "Todo 4", imagePath: codingImage },
+]
